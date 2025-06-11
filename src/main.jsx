6beaa21a129583b0f5f3a14b7a6c02581a37f9e0
Elina-Sharlai <elinakometa@gmail.com>
2025-06-11 18:35:13 +0300

    eighth module

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import './reset.css';
-import 'modern-normalize';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import App from "./components/App/App";
+import { store, persistor } from "./redux/store";
+import "modern-normalize";
+import "./index.css";
 
-import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
